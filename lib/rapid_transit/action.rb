2a58b23c576f647f9b0a2930e1de9f54155b597a
Jay McAliley <jay.mcaliley@gmail.com>
2012-01-23 22:02:32 -0500

    remove old namespace

--- lib/rapid_transit/action.rb
+++ lib/rapid_transit/action.rb
@@ -1,6 +1,6 @@
 class RapidTransit::Action; end
 
-require 'parser/action/base'
-require 'parser/action/class_method'
-require 'parser/action/instance_method'
-require 'parser/action/setter'
+require 'rapid_transit/action/base'
+require 'rapid_transit/action/class_method'
+require 'rapid_transit/action/instance_method'
+require 'rapid_transit/action/setter'
